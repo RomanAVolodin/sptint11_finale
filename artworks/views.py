31c28625c7adcdad7aa195b0aff68ba02cbd3d72
Roman Volodin <32717824+RomanAVolodin@users.noreply.github.com>
2020-08-25 22:50:47 +0300

    artwork categories done

--- artworks/views.py
+++ artworks/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+from users.rbac import AnyoneCanSeeListAdminCanEdit
+
+
+class CategoriesViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = (AnyoneCanSeeListAdminCanEdit,)
+    queryset = Category.objects.all()
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
